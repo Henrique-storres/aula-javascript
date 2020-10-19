d4e4500e75d13b7f0e1327dee0b629e1f32078aa
Henrique S. Torres <storres.henrique@gmail.com>
2020-10-19 11:49:37 -0300

    Fim Aula 01

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var nome = "Henrique Torres";
+var n1 = 26;
+var n2 = 10;
+var frase = "Japão é o melhor time do mundo"
+
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.replace("Japão","Brasil"));
