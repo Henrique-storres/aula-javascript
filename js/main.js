59ded701f959df273a54f46c12890937738ffd04
Henrique S. Torres <storres.henrique@gmail.com>
2020-10-19 12:20:04 -0300

    Fim Aula 02

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
-var nome = "Henrique Torres";
-var n1 = 26;
-var n2 = 10;
-var frase = "Japão é o melhor time do mundo"
 
-console.log(nome);
-console.log(n1 * n2);
-console.log(frase.replace("Japão","Brasil"));
+/*
+var d = new Date();
+alert(d.getMonth());
+*/
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Informe sua idade");
+if (idade > 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
