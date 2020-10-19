17efa73b873537db3f3817b0ae0b4f33a992d350
Henrique S. Torres <storres.henrique@gmail.com>
2020-10-19 14:05:23 -0300

    Aula 03 mudanças

--- js/main.js
+++ js/main.js
@@ -1,3 +1,56 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por Clicar";
+}
+
+function redirecionar(){
+    window.open('https://github.com/Henrique-storres/aula-javascript');
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+
+
+
+
+
+/*
+function soma(n1,n2){
+    return n1 + n2;
+}
+
+
+/*
+var validar;
+
+function validaIdade(idade){
+
+    if (idade > 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Informe sua Idade");
+console.log(validaIdade(idade));
+
+
+alert(soma(5, 10));
+
+*/
+
+
 
 /*
 var d = new Date();
